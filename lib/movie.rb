eb0bf50e49fe60c14373c84046402f3986fd1e43
Arek Turlewicz <aturlewicz@mediweb.jp>
2018-06-08 13:18:47 +0900

    Replace type code with state/strategy.

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,3 +1,25 @@
+class RegularPrice
+  def charge(days_rented)
+  	result = 2
+  	result += (days_rented - 2) * 1.5  if days_rented > 2
+	result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+  	days_rented * 3
+  end
+end
+
+class ChildrensPrice
+  def charge(days_rented)
+  	result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
+  end
+end
+
 class Movie
   REGULAR = 0
   NEW_RELEASE = 1
@@ -7,21 +29,19 @@ class Movie
   attr_reader :price_code
 
   def initialize(title, price_code)
-    @title, @price_code = title, price_code
+    @title, self.price_code = title, price_code
+  end
+
+  def price_code=(price_code)
+  	@price_code = price_code
+  	@price = case price_code
+    when REGULAR then RegularPrice.new
+    when NEW_RELEASE then NewReleasePrice.new
+    when CHILDRENS then ChildrensPrice.new
+    end
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 end
